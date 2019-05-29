41ad19b4e7111ddd54d502f0cff952036f2c51d9
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-28 22:32:36 -0600

    update

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
-constructor() {
-  super();
+class App extends Component {
+  constructor() {
+    super();
 
-  this.state = {
-    foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
-  };
+    this.state = {
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    };
+  }
+  render() {
+    let foodsToDisplay = this.state.foods.map((element, index) => {
+      return <h2 key={index}>{element}</h2>;
+    });
+
+    return <div className="App">{foodsToDisplay}</div>;
+  }
 }
 
-render() {
-  let foodsToDisplay = this.state.foods.map((element, index) => {
-    return <h2 key={index}>{element}</h2>;
-  });
 
-  return <div className="App">{foodsToDisplay}</div>;
-}
-}
 
 export default App;
