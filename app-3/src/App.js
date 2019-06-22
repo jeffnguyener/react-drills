2e8b6c92b2e97ab283fb578593ac98ad958d26a0
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-22 14:31:27 -0600

    load button works

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import DangerButton from './Components/DangerButton'
+
 
 class App extends Component {
+  handleClick = () => {
+    import('./Components/moduleA')
+      .then(({ moduleA }) => {
+        console.log(moduleA)
+        // Use moduleA
+      })
+      .catch(err => {
+        // Handle failure
+      });
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.handleClick}>Load</button>
+        <DangerButton />
       </div>
     );
   }
---
+++ app-3/src/Components/DangerButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Button from './Button'
+
+export default class DangerButton extends Component {
+    render() {
+        return (
+            <div>
+                <Button color="red" />
+            </div>
+        )
+    }
+}
