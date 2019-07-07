69be435470635cd22100b9671fd29187d8ab0f26
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-07-07 12:38:46 -0600

    app 3 finished

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import DangerButton from './Components/DangerButton'
-
 
 class App extends Component {
-  handleClick = () => {
-    import('./Components/moduleA')
-      .then(({ moduleA }) => {
-        console.log(moduleA)
-        // Use moduleA
-      })
-      .catch(err => {
-        // Handle failure
-      });
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      filterString: "",
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    };
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter });
+  }
 
   render() {
+    let foodsToDisplay = this.state.foods
+      .filter((element, index) => {
+        return element.includes(this.state.filterString);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      });
+
     return (
-      <div>
-        <button onClick={this.handleClick}>Load</button>
-        <DangerButton />
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {foodsToDisplay}
       </div>
     );
   }
